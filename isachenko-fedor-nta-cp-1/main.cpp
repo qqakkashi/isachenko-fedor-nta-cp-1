141b25a3218ea02649ff48113d97abbe374d9a37
Fedor Isachenko <qqakkashi@gmail.com>
2023-02-19 18:10:40 +0200

    Initial Commit

---
+++ isachenko-fedor-nta-cp-1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  isachenko-fedor-nta-cp-1
+//
+//  Created by qqakkashi on 19.02.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
